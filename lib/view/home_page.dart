21804485b639d6704a1d4e186921d09fb9c53367
afharyo <afharyodiwseno@gmail.com>
2025-05-01 16:36:34 +0700

    Initial commit

---
+++ lib/view/home_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:wibu_app/controller/anime_presenter.dart';
+import 'package:wibu_app/model/anime_model.dart';
+
+class AnimeListScreen extends StatefulWidget {
+  const AnimeListScreen({super.key});
+
+  @override
+  State<AnimeListScreen> createState() => _AnimeListScreenState();
+}
+
+class _AnimeListScreenState extends State<AnimeListScreen> implements AnimeView {
+  late AnimePresenter _presenter;
+  bool _isLoading = false;
+  List<Anime> _animeList = [];
+  String? _errorMessage;
+  String _currentEndpoint = "akatsuki";
+
+  @override
+  void initState(){
+    super.initState();
+    _presenter = AnimePresenter(this);
+    _presenter.loadAnimeData(_currentEndpoint);
+  }
+
+  void fetchData(String endpoint){
+    _currentEndpoint = endpoint;
+    _presenter.loadAnimeData(_currentEndpoint);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Anime List"),),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: () {}, child: Text("Akatsuki")),
+              SizedBox(width: 10,),
+              ElevatedButton(onPressed: () {}, child: Text("Kara")),
+              
+            ],
+          ),
+          Expanded(child: _isLoading ?
+          Center(child: CircularProgressIndicator())
+              : _errorMessage != null
+              ? Center(child: Text("Error Message"))
+              : ListView.builder(
+              itemCount: _animeList.length,
+              itemBuilder: (context,index){
+                final anime = _animeList[index];
+                return ListTile(
+                  leading: anime.imageUrl.isNotEmpty ? Image.network(anime.imageUrl) : Image.network('https://placehold.co/600x400'),
+                  title: Text(anime.name),
+                  subtitle: Text("Family ${anime.familyCreator}"),
+                );
+              })
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  void hideLoading() {
+    // TODO: implement hideLoading
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void showAnimeList(List<Anime> animeList) {
+    // TODO: implement showAnimeList
+    setState(() {
+      _animeList = animeList;
+    });
+  }
+
+  @override
+  void showError(String message) {
+    // TODO: implement showError
+    setState(() {
+      _errorMessage = message;
+    });
+  }
+
+  @override
+  void showLoading() {
+    // TODO: implement showLoading
+    _isLoading = true;
+  }
+}
